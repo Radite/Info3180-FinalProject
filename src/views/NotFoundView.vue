5bfefecf6edf2761c8ba558c5cf473ae32491859
Radite <joshuameghoo@gmail.com>
2024-04-11 15:40:29 -0400

    Default 404 Page

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>404 - Page Not Found</h1>
+    <p>The requested page could not be found.</p>
+    <!-- Add additional content or styling as needed -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView'
+}
+</script>
+
+<style scoped>
+/* Add component-specific styles here */
+</style>
